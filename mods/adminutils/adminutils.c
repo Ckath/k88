c279a4ff6397ece9c43fc64562b7a6755931a6f4
ckath <ckat@teknik.io>
2020-06-27 00:51:39 +0200

    add admin utils beginning
    for now only a simple way to run commands on host machine for mod

---
+++ mods/adminutils/adminutils.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include "../../ini_rw/ini_rw.h"
+
+/* required */
+#include "../modtape.h"
+#include "../../modules.h"
+#include "../../irc.h"
+
+static void
+handle_cmdmsg(
+		irc_conn *s, char *index, char *chan, char *user, char *msg, bool mod)
+{
+	/* admin only */
+	if (!mod) {
+		return;
+	}
+
+	if (!strncmp(msg, "shell ", 6)) {
+		char *cmd = strchr(msg, ' ')+1;
+		FILE *cp = popen(cmd, "r");
+		char output[500] = { '\0' };
+		fgets(output, 499, cp);
+		if (output[0]) {
+			send_raw(s, 0, "PRIVMSG %s :%s\r\n", DEST, output);
+		}
+		pclose(cp);
+	}
+}
+
+void
+adminutils_init()
+{
+	mods_new("adminutils", true);
+	mods_cmdmsg_handler(handle_cmdmsg);
+}

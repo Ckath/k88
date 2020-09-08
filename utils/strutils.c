270035d85cd7139452ed947541a0add833be51f8
ckath <ckat@teknik.io>
2020-09-09 00:25:37 +0200

    remove worthless lower code
    tell module is made so well it doesnt matter if names arent
    lowered here

--- utils/strutils.c
+++ utils/strutils.c
@@ -18,14 +18,3 @@ strrplc(char *haystack, char *needle, char *replace)
 	}
 	return matches;
 }
-
-char *
-strlower(char *str)
-{
-	for (int i = 0; str[i]; ++i) {
-		if (str[i] >= 'A' && str[i] <= 'Z') {
-			str[i] += 32;
-		}
-	}
-	return str;
-}

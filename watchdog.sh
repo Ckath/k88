10ad1d3ced07673b4be9458d9b640b0f283d2e49
mssx86 <mss@tutanota.de>
2021-01-12 03:08:01 +0300

    {modcodegen,watchdog}.sh, remove bashisms.

--- watchdog.sh
+++ watchdog.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # bot isnt running yet, reset watchdog file
-touch /tmp/k88_alive
+> /tmp/k88_alive
 
 # reset bot after it hasnt touched watchdog file for 3 minutes
 while true; do
-	[ $(($(date +%s)-$(stat -c %Y /tmp/k88_alive))) -gt 180 ] && echo "watchdog timeout" > /tmp/k88_crash && pkill k88
+	[ "$(($(date +%s)-$(stat -c %Y /tmp/k88_alive)))" -gt 180 ] && echo "watchdog timeout" > /tmp/k88_crash && pkill k88
 	sleep 1
 done

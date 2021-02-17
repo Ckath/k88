0bcc94366ce7b95cf53d79a2ffc873923b46e07d
ckath <ckat@teknik.io>
2021-02-17 22:38:04 +0100

    fix typo in error log

--- core/log.h
+++ core/log.h
@@ -5,6 +5,6 @@
 #define log_recv(logformat, ...) printf("[ \033[34m<<<\033[39m ] " logformat, ##__VA_ARGS__)
 #define log_send(logformat, ...) printf("[ \033[32m>>>\033[39m ] " logformat, ##__VA_ARGS__)
 #define log_info(logformat, ...) printf("[ \033[33m(!)\033[39m ] " logformat, ##__VA_ARGS__)
-#define log_err(logformat, ...) fprintf(stderr, "[ \032[31m!!!\033[39m ] " logformat, ##__VA_ARGS__)
+#define log_err(logformat, ...) fprintf(stderr, "[ \033[31m!!!\033[39m ] " logformat, ##__VA_ARGS__)
 
 #endif
